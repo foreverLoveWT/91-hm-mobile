460541b81b6a737447948420cfc8ff3ec74ecfde
foreverLoveWT <l>
2020-01-14 21:25:12 +0800

    时间格式化 过滤器

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,13 +1,25 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/zh-cn'
+
+dayjs.extend(relativeTime)
+
 export default {
   install (Vue) {
     Vue.prototype.$mynotify = (params) => Vue.prototype.$notify({ duration: 800, ...params })
     Vue.prototype.$sleep = sleep
+    Vue.filter('relTime', relTime)
   }
 }
 
 // 封装延迟处理函数
 function sleep (time = 500) {
-  return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve) {
     setTimeout(() => resolve(), time)
   })
+}
+
+// 时间格式化
+function relTime (value) {
+  return dayjs().locale('zh-cn').from(value)
 }
