f1bbe1d8945ff30cba0c4ed0b2b21884d43b0773
foreverLoveWT <l>
2020-01-15 20:50:30 +0800

    搜索基本布局完成

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,14 +1,73 @@
 <template>
   <div class="container">
+    <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
+    <!-- 导航 -->
+    <van-search v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
+    <!-- trim去掉空格   -->
+    <van-cell-group class="suggest-box" v-if="q">
+      <van-cell icon="search">
+        <span>j</span>ava
+      </van-cell>
+    </van-cell-group>
+    <div class="history-box" v-else>
+      <div class="head">
+        <span>历史记录</span>
+        <van-icon name="delete"></van-icon>
+      </div>
+      <van-cell-group>
+        <van-cell>
+          <a class="word_btn">电脑</a>
+          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        </van-cell>
+      </van-cell-group>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      q: ''
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped>
+<style lang='less' scoped>
+.history-box {
+  padding: 0 20px;
+  .head{
+    line-height: 36px;
+    color: #999;
+    .van-icon{
+      font-size: 16px;
+      float: right;
+      margin-top: 10px;;
+    }
+  }
+  .van-cell{
+    padding: 10px 0;
+  }
+  .word_btn{
+    color:#3296fa;
+  }
+  .close_btn{
+    margin-top:5px;
+    color: #999;
+  }
+}
+.suggest-box{
+  /deep/ .van-cell{
+    padding: 10px 20px;
+    color: #999;
+    p{
+      span{
+        color: red;
+      }
+    }
+  }
+}
 </style>
