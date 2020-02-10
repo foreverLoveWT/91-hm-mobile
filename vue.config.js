b3b40690c2b0075a1edeeac86e159c8ef3fa6ab2
foreverLoveWT <l>
2020-02-10 15:13:26 +0800

    打包完成

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,11 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+    }
+  },
+  publicPath: './'
 }
